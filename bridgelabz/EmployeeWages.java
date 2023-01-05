3558b802340af98d48e4eceaca4b23752b456d41
bhartifule <116824470+bhartifule@users.noreply.github.com>
2023-01-05 12:23:39 +0530

    Update EmployeeWages.java

--- bridgelabz/EmployeeWages.java
+++ bridgelabz/EmployeeWages.java
@@ -3,12 +3,9 @@ package com.bridgelabz;
 public class EmployeeWages {
 public static void main(String[] args) {
 	System.out.println("Welcome to Employee Wages");
-	//0.123 ,0.3423 , 0.567 ,0.423
-	//2.23 ,3.423 ,3.67 ,4.23
-	//1.0 ,3.0 ,5.0 ,4.0
 	
-	//double random = Math.random();
-//	System.out.println("random");
+	double random = Math.random();
+	// System.out.println("random");
 	int employeeChek = (int) Math.floor(Math.random()*10) % 2;
 	System.out.println("employeeChek");
 	if(employeeChek == 1) {
