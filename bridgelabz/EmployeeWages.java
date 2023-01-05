3f2f5dc2f8b7e95095c28684ef7f1acc05d68d4f
bhartifule <116824470+bhartifule@users.noreply.github.com>
2023-01-05 12:34:05 +0530

    Update EmployeeWages.java

--- bridgelabz/EmployeeWages.java
+++ bridgelabz/EmployeeWages.java
@@ -3,12 +3,13 @@ package com.bridgelabz;
 public class EmployeeWages {
 public static void main(String[] args) {
 	System.out.println("Welcome to Employee Wages");
-	
+       int IS_FULL_TIME = 1;
+
 	//double random = Math.random();
 	// System.out.println("random");
-	int employeeChek = (int) Math.floor(Math.random()*10) % 2;
+	double employeeChek =  Math.floor(Math.random()*10) % 2;
 	System.out.println("employeeChek");
-	if(employeeChek == 1) {
+	if(employeeChek == IS_FULL_TIME) {
 		System.out.println("Employee is Present");
 	}else
 	{
