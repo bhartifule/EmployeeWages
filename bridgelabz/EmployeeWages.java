07c8c45176ddccd8d373d97867f1f18d8b871452
bhartifule <116824470+bhartifule@users.noreply.github.com>
2023-01-05 13:36:45 +0530

    Update EmployeeWages.java

--- bridgelabz/EmployeeWages.java
+++ bridgelabz/EmployeeWages.java
@@ -73,79 +73,16 @@ public class EmployeeWages {
 
 		}
 
-		// UC-5----------------
-		void employeeMonthlyWage() {
-			int fullTime = 2;
-			int partTime = 1;
-			int employeeWagePerHour = 20;
-			int workingDaysPerMonth = 20;
-			int employeeHrs = 0;
-			int employeeWage = 0;
-			int monthlyWage = 0;
-			for (int day = 0; day < workingDaysPerMonth; day++) {
-				int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
-				switch (employeeCheck) {
-				case 1:
-					employeeHrs = 16;
-					break;
-				case 2:
-					employeeHrs = 8;
-					break;
-				default:
-					employeeHrs = 0;
-
-				}
-				employeeWage = employeeHrs * employeeWagePerHour;
-				monthlyWage += employeeWage;
-				System.out.println("Employee wage:" + employeeWage);
-			}
-			System.out.println("Total Employee Wage " + monthlyWage);
-		}
-
-	//UC-6----------
-		void employeeMonthlyWageWhileloop() {
-				
-					int employeeWagePerHour = 20;
-					int workingDaysPerMonth = 20;
-					int employeeHrs = 0;
-					int employeeWage = 0;
-					int totalEmployeeHours=0;
-					int maximumHoursInMonth=100;
-					int totalWorkingDays=0;
-					while(totalEmployeeHours<= maximumHoursInMonth &&
-							totalWorkingDays<workingDaysPerMonth) {
-						totalWorkingDays++; {
-						int employeeCheck=(int)Math.floor(Math.random() *10) %3;
-						 switch(employeeCheck) {
-						 case 1 :
-							 employeeHrs=16;
-							 break;
-						 case 2 :
-							 employeeHrs=8;
-							 break;
-							 default:
-								 employeeHrs=0;
-							 
-						 }
-						 employeeWage = employeeHrs * employeeWagePerHour;
-						
-						totalEmployeeHours += employeeHrs;
-						 System.out.println("Day#: " + totalWorkingDays + " EmpHour: " +employeeHrs);
-					}
-					int totalEmployeeWage= totalEmployeeHours * employeeWagePerHour;
-					System.out.println("Total Employee Wage " +totalEmployeeWage);
-					}
-		}	
-					
+		
 
 		public static void main(String[] args) {
 			EmployeeWages Emp = new EmployeeWages();
 			// Emp.checkEmployeeAttendence();
 			// Emp.dailyEmployeeWage();
 			// Emp.dailyEmployeePartTimeWage();
-			//Emp.employeeMonthlyWage();
-			// System.out.println(Emp.switchcase());
-			Emp.employeeMonthlyWageWhileloop();
+
+			 System.out.println(Emp.switchcase());
+	
 		}
 	
 }
